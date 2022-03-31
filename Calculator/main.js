5c7adf5522eaacf3f45249d314ea383b99e2cca1
amar0020 <amarkrsoni8989@gmail.com>
2022-03-31 20:09:30 +0530

    introduction to node

---
+++ Calculator/main.js
@@ -0,0 +1,14 @@
+const add = require("./add")
+
+const subtract = require("./subtract")
+const multiplpy = require("./multiply")
+const divide = require("./divide")
+
+
+console.log(add(4,5))
+console.log(subtract(4,5))
+console.log(multiplpy(4,5))
+
+console.log(divide(4,5))
+
+
